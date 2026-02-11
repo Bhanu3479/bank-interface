e416091bc1378b18f1112ce1770403746560ddb7
Bhanu3479 <bhanuprakash3479@gmail.com>
2026-02-11 22:59:54 +0530

    Update axios.js

--- bank-system/src/api/axios.js
+++ bank-system/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://bank-backend-7uqn.onrender.com",
 });
 
-// Attach token automatically
 API.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem("user"));
   const managerToken = localStorage.getItem("managerToken");
