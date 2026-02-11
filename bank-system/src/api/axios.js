111388ff157230e8e96b7b0ee9ff3f3283364080
Bhanu3479 <bhanuprakash3479@gmail.com>
2026-02-11 18:23:22 +0530

    Change API base URL to production server

--- bank-system/src/api/axios.js
+++ bank-system/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://bank-backend-7uqn.onrender.com",
 });
 
 // Attach JWT automatically (User OR Manager)
